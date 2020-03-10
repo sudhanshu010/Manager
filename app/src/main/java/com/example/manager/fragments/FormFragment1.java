5b351cf8e5fc5c9ac050ca01b1d848bdd2167f30
Abhinendra <abhinendragaurav@gmail.com>
2020-03-10 22:31:12 +0530

    Generate qr page modified

---
+++ app/src/main/java/com/example/manager/fragments/FormFragment1.java
@@ -0,0 +1,32 @@
+package com.example.manager.fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.manager.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FormFragment1 extends Fragment {
+
+
+    public FormFragment1() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_form_1, container, false);
+    }
+
+}
