f39fd4a42563be68fa0b5b365184684c89e95e43
Abhinendra <abhinendragaurav@gmail.com>
2020-03-11 17:49:02 +0530

    bottom nav changed

--- app/src/main/java/com/example/manager/BottomNavigationActivity.java
+++ app/src/main/java/com/example/manager/BottomNavigationActivity.java
@@ -3,6 +3,7 @@ package com.example.manager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,6 +12,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 import com.example.manager.fragments.HistoryFragment;
 import com.example.manager.fragments.HomeFragment;
 import com.example.manager.fragments.NotificationFragment;
@@ -19,41 +21,70 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class BottomNavigationActivity extends AppCompatActivity {
 
-    private BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setOurFragment(new HomeFragment());
         setContentView(R.layout.activity_bottom_navigation);
-        bottomNavigationView = findViewById(R.id.bottom_bar);
-        bottomNavigationView.setItemIconTintList(null);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        MeowBottomNavigation bottomNavigation = findViewById(R.id.bottom_bar);
+
+        bottomNavigation.add(new MeowBottomNavigation.Model(1, R.drawable.ic_home));
+        bottomNavigation.add(new MeowBottomNavigation.Model(2, R.drawable.ic_history));
+        bottomNavigation.add(new MeowBottomNavigation.Model(3, R.drawable.ic_notification1));
+        bottomNavigation.add(new MeowBottomNavigation.Model(4, R.drawable.ic_account));
+
+        bottomNavigation.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId())
-                {
-                    case R.id.home:
+            public void onClickItem(MeowBottomNavigation.Model item) {
+
+                switch (item.getId()) {
+                    case 1:
                         setOurFragment(new HomeFragment());
-                        return true;
-
-                    case R.id.history:
+                        break;
+                    case 2:
                         setOurFragment(new HistoryFragment());
-                        return true;
-
-                    case R.id.notification:
+                        break;
+                    case 3:
                         setOurFragment(new NotificationFragment());
-                        return true;
-
-                    case R.id.profile:
+                        break;
+                    case 4:
                         setOurFragment(new ProfileFragment());
-                        return true;
-
-                    default:
-                        return false;
                 }
 
             }
         });
+        bottomNavigation.show(1,true);
+
+
+//        bottomNavigationView.setItemIconTintList(null);
+//        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                switch (item.getItemId())
+//                {
+//                    case R.id.home:
+//                        setOurFragment(new HomeFragment());
+//                        return true;
+//
+//                    case R.id.history:
+//                        setOurFragment(new HistoryFragment());
+//                        return true;
+//
+//                    case R.id.notification:
+//                        setOurFragment(new NotificationFragment());
+//                        return true;
+//
+//                    case R.id.profile:
+//                        setOurFragment(new ProfileFragment());
+//                        return true;
+//
+//                    default:
+//                        return false;
+//                }
+//
+//            }
+//        });
     }
     private void setOurFragment(Fragment fragment)
     {
