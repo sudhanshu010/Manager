14604f734a11f0c660a1fe68cbe733f46a414f77
Abhinendra <abhinendragaurav@gmail.com>
2020-07-03 22:11:33 +0530

    UI

--- app/src/main/java/com/example/manager/MainActivity.java
+++ app/src/main/java/com/example/manager/MainActivity.java
@@ -1,20 +1,44 @@
 package com.example.manager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.manager.DialogBox.ComplaintDescriptionDialog;
 import com.example.manager.models.Complaint;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView phoneTV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//        phoneTV = findViewById(R.id.phone_tv);
+//        if (Build.VERSION.SDK_INT > 22) {
+//
+//            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+//
+//                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE}, 101);
+//
+//                return;
+//            }
+//            Intent callIntent = new Intent(Intent.ACTION_CALL);
+//            callIntent.setData(Uri.parse("tel:+" + phoneTV.getText().toString().trim()));
+//            startActivity(callIntent);
+//        } else {
+//
+//            Intent callIntent = new Intent(Intent.ACTION_CALL);
+//            callIntent.setData(Uri.parse("tel:+" + phoneTV.getText().toString().trim()));
+//            startActivity(callIntent);
+//        }
     }
 }
