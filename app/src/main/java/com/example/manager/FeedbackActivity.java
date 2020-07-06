c6617ca676c7f3a9b31da205f5b1a58b4435dca2
Abhinendra <abhinendragaurav@gmail.com>
2020-07-06 11:42:25 +0530

    Profile Updated and Horizontal Stepper Renewed

--- app/src/main/java/com/example/manager/FeedbackActivity.java
+++ app/src/main/java/com/example/manager/FeedbackActivity.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.material.chip.Chip;
+
 public class FeedbackActivity extends AppCompatActivity {
 
+    Chip chip1,chip2,chip3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +19,8 @@ public class FeedbackActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         toolbar.setTitleTextAppearance(this,R.style.TitleTextAppearance);
+
+
     }
     @Override
     public boolean onSupportNavigateUp() {
