9a33416c1af8237fd57f1c86507aae5048498793
Vikas Goyal <vikasg.cs.17@nitj.ac.in>
2020-08-03 10:56:57 +0530

    request decline problem solved

--- app/src/main/java/com/example/manager/LoginActivity.java
+++ app/src/main/java/com/example/manager/LoginActivity.java
@@ -191,7 +191,7 @@ public class LoginActivity extends AppCompatActivity {
 
                                             customDialogBox.dismiss();
 //                                            SweetToast.info(LoginActivity.this,"Success");
-                                            Intent i = new Intent(LoginActivity.this,VerifyActivity.class);
+                                            Intent i = new Intent(LoginActivity.this,BottomNavigationActivity.class);
                                             i.putExtra("Email",email);
                                             i.putExtra("Password",password);
                                             startActivity(i);
